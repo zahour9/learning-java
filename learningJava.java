c91bed096c602992e189876250f3f02e2edb7f7c
Abderrahim ZAHOUR <abderrahim.zahour@ma.ibm.com>
2024-10-08 18:18:44 +0100

    fix: resolving issues in the code

--- learningJava.java
+++ learningJava.java
@@ -1,13 +1,16 @@
 public class learningJava {
     public static void main(String[] args) {
-        double[] array = {1, 2, 3};
+        double[] array = {1000039.001, 1.22, -10.909};
         double bigNum = getBigNum(array);
         System.out.println(bigNum);
     }
     public static double getBigNum(double[] arr){
         double num = 0;
         for(int i = 0; i < arr.length; i++){
-            if(arr[i] > 0){
+            if(i == 0){
+            num = arr[0];
+            }
+            if(arr[i] > num){
                 num = arr[i];
             }
         }
