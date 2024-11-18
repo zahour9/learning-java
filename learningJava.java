c895a2be58b2d2cc9732f5f88634c66109f2ca7d
Abderrahim ZAHOUR <abderrahim.zahour@ma.ibm.com>
2024-11-18 12:20:49 +0100

    feat: add even condition

--- learningJava.java
+++ learningJava.java
@@ -3,7 +3,12 @@ public class learningJava {
         double num = arr[0];
         for(int i = 0; i < arr.length; i++){
             if(arr[i] > num){
+             if(num % 2 == 0){
                 num = arr[i];
+             }
+             else {
+                num = 0;
+             }
             }
         }
         return num;
--- learningJavaTest.java
+++ learningJavaTest.java
@@ -2,9 +2,18 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class learningJavaTest {
+    public boolean isEven(double num){
+        if(num % 2 == 0){
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
     @Test
     public void testGetBigNum(){
         double[] array = {-19595954.0002, 19595954.0001, -3, 40404};
         assertEquals(learningJava.getBigNum(array), 19595954.0001, 0.0001);
+        assertEquals(isEven(learningJava.getBigNum(array)), true);
 }
 }
