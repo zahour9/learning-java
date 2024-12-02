e0038a7111f46fe3a0e0a2d7c4d102ae8a731a73
Abderrahim ZAHOUR <abderrahim.zahour@ma.ibm.com>
2024-12-02 16:03:55 +0100

    adding new tests

--- dataCalculatorTest.java
+++ dataCalculatorTest.java
@@ -7,6 +7,7 @@ public class dataCalculatorTest {
         assertEquals(dateCalculator.dateMethod("8/1/2024", "12/04/2024", false), "The number of days between 8/1/2024 and 12/04/2024 is: 69");
         assertEquals(dateCalculator.dateMethod("8/1/2024", "12/04/2024", true), "The number of days between 8/1/2024 and 12/04/2024 is: 95");
         assertEquals(dateCalculator.dateMethod("8/14/2024", "2024", true), "Date form should be like that: dd/MM/yyyy");
-
+        assertEquals(dateCalculator.dateMethod("0/0/2023", "1/2/2023", true), "Date form should be like that: dd/MM/yyyy");
+        assertEquals(dateCalculator.dateMethod("5/1/2012", "6/7/2013", true), "The number of days between 5/1/2012 and 6/7/2013 is: 548");
     }
 }
